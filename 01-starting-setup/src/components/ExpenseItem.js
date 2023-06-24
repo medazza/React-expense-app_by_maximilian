afff9755dcc197e6b09521ccee07a8113ca4e78d
medazza <midazza27@gmail.com>
2023-06-24 11:27:31 +0100

    first use of props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return(<p>this is my first component</p>);
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
+    return(
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
